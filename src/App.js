e438d41f342d4f9c12a92a5726ed84fc6b34f459
AlefMemTav <190115548@aluno.unb.br>
2022-12-02 18:29:08 -0300

    Configurado back-end e front-end com create-react-app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Receitalista
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/mdsreq-fga-unb/2022.2-Receitalista"
+        >
+          Repositório
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
