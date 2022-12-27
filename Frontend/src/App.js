51ba4f440c123e5f9895bd7894a7820548f99bc0
pehenobra2 <peheguibra@gmail.com>
2022-12-27 18:40:18 -0300

    Criação da sidebar e adição de alguns outros arquivos

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom"
 import Header from './components/Header/Header';
+import Navbar from "./components/Sidebar/NavBar";
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+       
+      <Navbar />
       <Outlet />
     </div>
   );
