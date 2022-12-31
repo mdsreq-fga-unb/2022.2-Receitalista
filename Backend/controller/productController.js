d19fddba417ee06a19bfe4fdfd97a79089fa42a7
valdersonjr <valdersonpontes@gmail.com>
2022-12-31 12:27:58 -0300

    [Add] Update product

--- Backend/controller/productController.js
+++ Backend/controller/productController.js
@@ -34,3 +34,315 @@ exports.addProduct = async function (req, res) {
             }
         })
 }
+
+// exports.updateProduct = async function (req, res) {
+//     const id = req.params.id;
+//     const { description, used_quantity, itens_price, total_price } = req.body;
+
+//     Product.update(
+//         { description: description, used_quantity: used_quantity, itens_price: itens_price, total_price: total_price },
+//         { where: { id: id } }
+//     )
+//     .then(result => {
+//         console.log(result.dataValues);
+//         return res.status(201).json({
+//             message: 'Product updated'
+//         });
+//     })
+//     .catch(err => {
+//         return res.status(500).json({
+//             err: err
+//         });
+//     });
+// }
+
+exports.updateProduct = async function (req, res) {
+    const id = req.params.id;
+    const { description, used_quantity, itens_price, total_price } = req.body;
+
+    if(description && !used_quantity && !itens_price && !total_price){
+        Product.update(
+            { description: description },
+            { where: { id: id }}
+        )
+        .then(result => {
+            console.log(result.dataValues);
+            return res.status(201).json({
+                message: 'Product description updated'
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500).json({
+                err: err
+            });
+        });
+    }
+
+    else if(!description && used_quantity && !itens_price && !total_price){
+        Product.update(
+            { used_quantity: used_quantity },
+            { where: { id: id }}
+        )
+        .then(result => {
+            console.log(result.dataValues);
+            return res.status(201).json({
+                message: 'Product used_quantity updated'
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500).json({
+                err: err
+            });
+        });
+    }
+
+    else if(!description && !used_quantity && itens_price && !total_price){
+        Product.update(
+            { itens_price: itens_price },
+            { where: { id: id }}
+        )
+        .then(result => {
+            console.log(result.dataValues);
+            return res.status(201).json({
+                message: 'Product itens_price updated'
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500).json({
+                err: err
+            });
+        });
+    }
+
+    else if(!description && !used_quantity && !itens_price && total_price){
+        Product.update(
+            { total_price: total_price },
+            { where: { id: id }}
+        )
+        .then(result => {
+            console.log(result.dataValues);
+            return res.status(201).json({
+                message: 'Product total_price updated'
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500).json({
+                err: err
+            });
+        });
+    }
+
+    else if(description && used_quantity && !itens_price && !total_price){
+        Product.update(
+            { description: description, used_quantity: used_quantity },
+            { where: { id: id }}
+        )
+        .then(result => {
+            console.log(result.dataValues);
+            return res.status(201).json({
+                message: 'Product description and used_quantity updated'
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500).json({
+                err: err
+            });
+        });
+    }
+
+    else if(description && !used_quantity && itens_price && !total_price){
+        Product.update(
+            { description: description, itens_price: itens_price },
+            { where: { id: id }}
+        )
+        .then(result => {
+            console.log(result.dataValues);
+            return res.status(201).json({
+                message: 'Product description and itens_price updated'
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500).json({
+                err: err
+            });
+        });
+    }
+
+    else if(description && !used_quantity && !itens_price && total_price){
+        Product.update(
+            { description: description, total_price: total_price },
+            { where: { id: id }}
+        )
+        .then(result => {
+            console.log(result.dataValues);
+            return res.status(201).json({
+                message: 'Product description and total_price updated'
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500).json({
+                err: err
+            });
+        });
+    }
+
+    else if(!description && used_quantity && itens_price && !total_price){
+        Product.update(
+            { used_quantity: used_quantity, itens_price: itens_price },
+            { where: { id: id }}
+        )
+        .then(result => {
+            console.log(result.dataValues);
+            return res.status(201).json({
+                message: 'Product used_quantity and itens_price updated'
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500).json({
+                err: err
+            });
+        });
+    }
+
+    else if(!description && used_quantity && !itens_price && total_price){
+        Product.update(
+            { used_quantity: used_quantity, total_price: total_price },
+            { where: { id: id }}
+        )
+        .then(result => {
+            console.log(result.dataValues);
+            return res.status(201).json({
+                message: 'Product used_quantity and total_price updated'
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500).json({
+                err: err
+            });
+        });
+    }
+
+    else if(!description && !used_quantity && itens_price && total_price){
+        Product.update(
+            { itens_price: itens_price, total_price: total_price },
+            { where: { id: id }}
+        )
+        .then(result => {
+            console.log(result.dataValues);
+            return res.status(201).json({
+                message: 'Product itens_price and total_price updated'
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500).json({
+                err: err
+            });
+        });
+    }
+
+    else if(description && used_quantity && itens_price && !total_price){
+        Product.update(
+            { description: description, used_quantity: used_quantity, itens_price: itens_price },
+            { where: { id: id }}
+        )
+        .then(result => {
+            console.log(result.dataValues);
+            return res.status(201).json({
+                message: 'Product description, used_quantity and itens_price updated'
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500).json({
+                err: err
+            });
+        });
+    }
+
+    else if(description && used_quantity && !itens_price && total_price){
+        Product.update(
+            { description: description, used_quantity: used_quantity, total_price: total_price },
+            { where: { id: id }}
+        )
+        .then(result => {
+            console.log(result.dataValues);
+            return res.status(201).json({
+                message: 'Product description, used_quantity and total_price updated'
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500).json({
+                err: err
+            });
+        });
+    }
+
+    else if(description && !used_quantity && itens_price && total_price){
+        Product.update(
+            { description: description, itens_price: itens_price, total_price: total_price },
+            { where: { id: id }}
+        )
+        .then(result => {
+            console.log(result.dataValues);
+            return res.status(201).json({
+                message: 'Product description, itens_price and total_price updated'
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500).json({
+                err: err
+            });
+        });
+    }
+
+    else if(!description && used_quantity && itens_price && total_price){
+        Product.update(
+            { used_quantity: used_quantity, itens_price: itens_price, total_price: total_price },
+            { where: { id: id }}
+        )
+        .then(result => {
+            console.log(result.dataValues);
+            return res.status(201).json({
+                message: 'Product used_quantity, itens_price and total_price updated'
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500).json({
+                err: err
+            });
+        });
+    }
+
+    else {
+        Product.update(
+            { description: description, used_quantity: used_quantity, itens_price: itens_price, total_price: total_price },
+            { where: { id: id }}
+        )
+        .then(result => {
+            console.log(result.dataValues);
+            return res.status(201).json({
+                message: 'Product description, used_quantity, itens_price and total_price updated'
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500).json({
+                err: err
+            });
+        });
+    }
+
+}
--- Backend/routes/productRouter.js
+++ Backend/routes/productRouter.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const ProductController = require('../controller/productController');
 
 router.post('/add', ProductController.addProduct);
+router.put('/:id', ProductController.updateProduct);
 
 module.exports = router;
