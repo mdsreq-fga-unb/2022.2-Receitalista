a79d3197803c370ad5b15615c260c40ebfb998d5
Lucas Gomes <lggomes007@gmail.com>
2023-01-04 22:56:01 -0300

    adicionando rota de get e getAll itens

--- Backend/routes/itemRouter.js
+++ Backend/routes/itemRouter.js
@@ -6,8 +6,12 @@ const ItemController = require('../controller/itemController');
 
 router.get('/list', checkAuth, ItemController.getAllItem);
 
+router.get('/:id', checkAuth, ItemController.getItem);
+
 router.post('/add', checkAuth, ItemController.addItem);
 
 router.put('/:id', checkAuth, ItemController.updateItem);
+
+router.delete('/:id', checkAuth, ItemController.deleteItem);
 
 module.exports = router;
