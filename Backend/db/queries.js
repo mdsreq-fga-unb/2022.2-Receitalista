a0cce6f35f9f4976fdef9bbe6344b1a3ab09eb92
Lucas Gomes <lggomes007@gmail.com>
2023-02-07 23:16:51 -0300

    adicionando rotas para pedidos

--- Backend/db/queries.js
+++ Backend/db/queries.js
@@ -2,8 +2,8 @@ const database = require("./connect");
 
 exports.connect = async function () {
     try {
-        await database.sync({ force: true });
-        // await database.sync();
+        // await database.sync({ force: true });
+        await database.sync();
         console.log(new Date().toISOString(), 'Connection has been established successfully.');
     } catch (error) {
         console.error(new Date().toISOString(), 'Unable to connect to the database:', error);
